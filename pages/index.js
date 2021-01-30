b21bc6c1abce80710f94e0c82e0e00ee9d3e0978
kbu715 <kbu715@gmail.com>
2021-01-30 13:51:44 +0900

    getStaticProps | fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({articles}) {
+  // console.log(articles)
   return (
     <>
     <Head>
       <title>WebDev News</title>
       <meta name="keywords" content="web development, programming" />
     </Head>
-    <h1>Welcome to Next</h1>
+    <ArticleList articles={articles}/>
     </>
   )
+}
+
+/* Next.js will pre-render this page at build time using the props returned by getStaticProps. */
+export const getStaticProps = async () => {
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+    const articles = await res.json();
+
+    return {
+      props: {
+        articles
+      }
+    }
 }
