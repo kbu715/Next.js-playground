de1af635598c86b9988e49c91a12dc0d11935f67
kbu715 <kbu715@gmail.com>
2021-01-30 15:46:46 +0900

    getStaticPaths with getStaticProps

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -6,17 +6,17 @@ const article = ({article}) => {
     // const {id} = router.query
 
     return (
-        <p>
+        <>
             <h1>{article.title}</h1>
             <p>{article.body}</p>
             <br />
             <Link href="/">Go Back</Link>
-        </p>
+        </>
     )
 }
 
 /* Next.js will pre-render this page on each request using the data returned by getServerSideProps. */
-export const getServerSideProps = async (context) => {
+/* export const getServerSideProps = async (context) => {
 
     // params: If this page uses a dynamic route, params contains the route parameters. If the page name is [id].js , then params will look like { id: ... }.
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
@@ -27,6 +27,57 @@ export const getServerSideProps = async (context) => {
             article
         }
     }
+} */
+
+
+
+
+/////////////////////////////////////////////////
+
+
+
+// each time we're going to these pages, we're fetching the data right with getServerSideProps
+// so we can also use a combination of getStaticProps and getStaticPaths to dynamically generate the paths with the data.
+// and doing it this way is going to make it much faster. because it fetched at build time.
+
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+
+// If you export an async function called getStaticPaths from a page that uses dynamic routes, Next.js will statically pre-render all the paths specified by getStaticPaths.
+export const getStaticPaths = async () => {
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const articles = await res.json()
+
+    const ids = articles.map(article => article.id)
+    const paths = ids.map(id => ( { params: { id: id.toString() } } ))
+
+    return {
+        paths,
+        fallback: false // it means if we go to something that doesn't exist in the data its going to return 404 page
+    }
 }
 
 export default article
+
+
+
+// note getStaticPaths
+
+/* return {
+    paths: [
+      { params: { id: '1' } },
+      { params: { id: '2' } }
+    ],
+    fallback: ...
+  } */
